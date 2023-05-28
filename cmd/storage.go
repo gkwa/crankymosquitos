3cc09325ac13f796b0c48919d697fdcb05c08098
Taylor Monacelli <taylormonacelli@gmail.com>
2023-05-27 23:03:23 -0700

    Good enuf

---
+++ cmd/storage.go
@@ -0,0 +1,315 @@
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"sort"
+	"strings"
+	"sync"
+
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+type ByStorageUsedEntity []EntityUsage
+
+func (a ByStorageUsedEntity) Len() int           { return len(a) }
+func (a ByStorageUsedEntity) Less(i, j int) bool { return a[i].StorageUsed > a[j].StorageUsed }
+func (a ByStorageUsedEntity) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+
+type EntityUsage struct {
+	ID               string
+	StorageUsed      int64
+	Region           string
+	IsVolume         bool
+	AttachedInstance string // New field to store the attached EC2 instance ID
+}
+
+var totalStorageUsed int64
+var (
+	entityMutex sync.Mutex
+	entities    []EntityUsage
+
+	ebsStorageUsed = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "ebs_storage_used",
+			Help: "EBS storage used by volume",
+		},
+		[]string{"volume_id", "region", "attached_instance"}, // Added "attached_instance" label
+	)
+
+	snapshotStorageUsed = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "snapshot_storage_used",
+			Help: "Snapshot storage used by snapshot",
+		},
+		[]string{"snapshot_id", "region", "attached_instance"}, // Added "attached_instance" label
+	)
+)
+
+func main() {
+	// Register the Prometheus metrics
+	prometheus.MustRegister(ebsStorageUsed)
+	prometheus.MustRegister(snapshotStorageUsed)
+
+	regions, err := GetAllAwsRegions()
+	if err != nil {
+		log.Fatalf("Failed to retrieve AWS regions: %v\n", err)
+	}
+
+	var wg sync.WaitGroup
+
+	// Launch goroutines to query volumes and snapshots concurrently
+	for _, region := range regions {
+		wg.Add(2)
+
+		go func(region string) {
+			log.Printf("Querying volumes in region: %s\n", region)
+
+			client, err := GetEc2Client(region)
+			if err != nil {
+				log.Printf("Failed to create EC2 client for region %s: %v\n", region, err)
+				wg.Done()
+				return
+			}
+
+			getEBSStorageUsed(client, region, &wg)
+		}(*region.RegionName)
+
+		go func(region string) {
+			log.Printf("Querying snapshots in region: %s\n", region)
+
+			client, err := GetEc2Client(region)
+			if err != nil {
+				log.Printf("Failed to create EC2 client for region %s: %v\n", region, err)
+				wg.Done()
+				return
+			}
+
+			getSnapshotStorageUsed(client, region, &wg)
+		}(*region.RegionName)
+	}
+
+	// Wait for all goroutines to complete
+	wg.Wait()
+
+	// Sort the entities by storage used in descending order
+	entityMutex.Lock()
+	sort.Sort(sort.Reverse(ByStorageUsedEntity(entities)))
+	entityMutex.Unlock()
+
+	for _, entity := range entities {
+		entityType := "Volume"
+		entityLink := ""
+		if !entity.IsVolume {
+			entityType = "Snapshot"
+			entityLink = fmt.Sprintf("https://%s.console.aws.amazon.com/ec2/home?region=%s#SnapshotDetails:snapshotId=%s",
+				strings.ToLower(entity.Region), entity.Region, entity.ID)
+		} else if entity.AttachedInstance == "" {
+			entityLink = fmt.Sprintf("https://%s.console.aws.amazon.com/ec2/home?region=%s#VolumeDetails:volumeId=%s",
+				strings.ToLower(entity.Region), entity.Region, entity.ID)
+		}
+
+		attachedInstance := entity.AttachedInstance
+		if attachedInstance == "" {
+			attachedInstance = "Not Attached" // Label indicating the volume/snapshot is not attached
+		}
+
+		output := fmt.Sprintf("%s ID: %s, Storage Used: %s, Region: %s, Attached Instance: %s",
+			entityType, entity.ID, formatBytes(entity.StorageUsed), entity.Region, attachedInstance)
+
+		if entityLink != "" {
+			output += fmt.Sprintf(", Link: %s", entityLink)
+		}
+
+		fmt.Println(output)
+	}
+
+	totalStorageUsedTB := float64(totalStorageUsed) / (1024 * 1024 * 1024 * 1024)
+	fmt.Printf("Total Storage Used: %.2f TB\n", totalStorageUsedTB)
+
+	// Start the Prometheus HTTP server
+	http.Handle("/metrics", promhttp.Handler())
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func formatBytes(bytes int64) string {
+	const unit = 1024
+	if bytes < unit {
+		return fmt.Sprintf("%d bytes", bytes)
+	}
+	div, exp := int64(unit), 0
+	for n := bytes / unit; n >= unit; n /= unit {
+		div *= unit
+		exp++
+	}
+	return fmt.Sprintf("%.0f GB", float64(bytes)/float64(div))
+}
+
+func getInstanceName(client *ec2.Client, instanceID string) string {
+	params := &ec2.DescribeInstancesInput{
+		InstanceIds: []string{instanceID},
+	}
+	resp, err := client.DescribeInstances(context.Background(), params)
+	if err != nil {
+		log.Printf("Failed to describe instances: %v\n", err)
+		return ""
+	}
+
+	if len(resp.Reservations) == 0 || len(resp.Reservations[0].Instances) == 0 {
+		log.Printf("No instance found with ID: %s\n", instanceID)
+		return ""
+	}
+
+	instance := resp.Reservations[0].Instances[0]
+	for _, tag := range instance.Tags {
+		if *tag.Key == "Name" {
+			return *tag.Value
+		}
+	}
+
+	return ""
+}
+
+func getVolumeName(client *ec2.Client, volumeID string) string {
+	params := &ec2.DescribeVolumesInput{
+		VolumeIds: []string{volumeID},
+	}
+	resp, err := client.DescribeVolumes(context.Background(), params)
+	if err != nil {
+		log.Printf("Failed to describe volumes: %v\n", err)
+		return ""
+	}
+
+	if len(resp.Volumes) == 0 {
+		log.Printf("No volume found with ID: %s\n", volumeID)
+		return ""
+	}
+
+	volume := resp.Volumes[0]
+	for _, tag := range volume.Tags {
+		if *tag.Key == "Name" {
+			return *tag.Value
+		}
+	}
+
+	return ""
+}
+
+func getEBSStorageUsed(client *ec2.Client, region string, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	params := &ec2.DescribeVolumesInput{}
+	resp, err := client.DescribeVolumes(context.Background(), params)
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			if aerr.Code() == "InvalidVolume.NotFound" {
+				// Handle the case when the volume does not exist
+				log.Printf("Invalid volume ID: %s\n", aerr.Message())
+				return
+			}
+		}
+
+		log.Printf("Failed to describe volumes in region %s: %v\n", region, err)
+		return
+	}
+
+	var volumes []EntityUsage
+
+	for _, volume := range resp.Volumes {
+		size := int64(*volume.Size) * 1024 * 1024 * 1024 // Convert from GB to bytes
+		totalStorageUsed += size
+
+		entity := EntityUsage{
+			ID:               *volume.VolumeId,
+			StorageUsed:      size,
+			Region:           region,
+			IsVolume:         true,
+			AttachedInstance: "", // Initialize the attached instance ID as empty
+		}
+
+		if volume.Attachments != nil && len(volume.Attachments) > 0 {
+			// Volume is attached to an instance
+			entity.AttachedInstance = *volume.Attachments[0].InstanceId
+
+			// Get instance name and replace instance ID with the tag "Name"
+			instanceName := getInstanceName(client, entity.AttachedInstance)
+			if instanceName != "" {
+				entity.AttachedInstance = instanceName
+			}
+		}
+
+		volumes = append(volumes, entity)
+
+		ebsStorageUsed.WithLabelValues(*volume.VolumeId, region, entity.AttachedInstance).Set(float64(size))
+	}
+
+	entityMutex.Lock()
+	entities = append(entities, volumes...)
+	entityMutex.Unlock()
+}
+
+func getSnapshotStorageUsed(client *ec2.Client, region string, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	params := &ec2.DescribeSnapshotsInput{
+		OwnerIds: []string{"self"},
+	}
+	resp, err := client.DescribeSnapshots(context.Background(), params)
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			if aerr.Code() == "InvalidSnapshot.NotFound" {
+				// Handle the case when the snapshot does not exist
+				log.Printf("Invalid snapshot ID: %s\n", aerr.Message())
+				return
+			}
+		}
+
+		log.Printf("Failed to describe snapshots in region %s: %v\n", region, err)
+		return
+	}
+
+	var snapshots []EntityUsage
+
+	for _, snapshot := range resp.Snapshots {
+		size := int64(*snapshot.VolumeSize) * 1024 * 1024 * 1024 // Convert from GB to bytes
+		totalStorageUsed += size
+
+		entity := EntityUsage{
+			ID:               *snapshot.SnapshotId,
+			StorageUsed:      size,
+			Region:           region,
+			IsVolume:         false,
+			AttachedInstance: "", // Snapshots are not attached to instances, so leave it empty
+		}
+
+		// Check if the snapshot has a "Name" tag
+		for _, tag := range snapshot.Tags {
+			if *tag.Key == "Name" {
+				entity.AttachedInstance = *tag.Value
+				break
+			}
+		}
+
+		// If the snapshot doesn't have a "Name" tag, check if the volume still exists and get its name
+		if entity.AttachedInstance == "" {
+			volumeID := *snapshot.VolumeId
+			volumeName := getVolumeName(client, volumeID)
+			if volumeName != "" {
+				entity.AttachedInstance = fmt.Sprintf("Volume: %s", volumeName)
+			}
+		}
+
+		snapshots = append(snapshots, entity)
+
+		snapshotStorageUsed.WithLabelValues(*snapshot.SnapshotId, region, entity.AttachedInstance).Set(float64(size))
+	}
+
+	entityMutex.Lock()
+	entities = append(entities, snapshots...)
+	entityMutex.Unlock()
+}
--- go.mod
+++ go.mod
@@ -3,21 +3,48 @@ module github.com/taylormonacelli/crankymosquitos
 go 1.20
 
 require (
+	github.com/aws/aws-sdk-go v1.44.271
+	github.com/aws/aws-sdk-go-v2 v1.18.0
+	github.com/aws/aws-sdk-go-v2/config v1.18.25
+	github.com/aws/aws-sdk-go-v2/service/ec2 v1.98.0
+	github.com/prometheus/client_golang v1.15.1
+	github.com/spf13/cobra v1.7.0
+	github.com/spf13/viper v1.15.0
+)
+
+require (
+	github.com/aws/aws-sdk-go-v2/credentials v1.13.24 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.13.3 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.33 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.27 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.3.34 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.9.27 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.12.10 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.14.10 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.19.0 // indirect
+	github.com/aws/smithy-go v1.13.5 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.6 // indirect
+	github.com/prometheus/client_model v0.3.0 // indirect
+	github.com/prometheus/common v0.42.0 // indirect
+	github.com/prometheus/procfs v0.9.0 // indirect
 	github.com/spf13/afero v1.9.3 // indirect
 	github.com/spf13/cast v1.5.0 // indirect
-	github.com/spf13/cobra v1.7.0 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/spf13/viper v1.15.0 // indirect
 	github.com/subosito/gotenv v1.4.2 // indirect
-	golang.org/x/sys v0.3.0 // indirect
-	golang.org/x/text v0.5.0 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/text v0.7.0 // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
